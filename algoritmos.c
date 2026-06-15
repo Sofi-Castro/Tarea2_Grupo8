e5ea88e6f26682d0d72df487a43d5f965e6fcea7
Sofi-Castro <castrosalassofia@gmail.com>
2026-06-15 17:44:19 -0600

    Archivos

---
+++ algoritmos.c
@@ -0,0 +1,58 @@
+/*
+ * algoritmos.c
+ * IE-0117 - Tarea en clase: Ordenamiento y Búsqueda
+ *
+ * Implementar las funciones asignadas en este archivo.
+ * No modificar algoritmos.h ni main.c.
+ */
+
+#include "algoritmos.h"
+
+/* ── Ordenamiento ─────────────────────────────────────── */
+
+void bubble_sort(int *arr, int n) {
+    (void)arr;
+    (void)n; 
+
+    for (int i=0; i > n, i)
+
+}
+
+void selection_sort(int *arr, int n) {
+    /* TODO: implementar */
+    (void)arr;
+    (void)n;
+}
+
+void insertion_sort(int *arr, int n) {
+    /* TODO: implementar */
+    (void)arr;
+    (void)n;
+}
+
+/* ── Búsqueda ─────────────────────────────────────────── */
+
+int busqueda_lineal(int *arr, int n, int valor) {
+    /* TODO: implementar */
+    (void)arr;
+    (void)n;
+    (void)valor;
+    return -1;
+}
+
+int busqueda_binaria(int *arr, int n, int valor) {
+    /* TODO: implementar */
+    (void)arr;
+    (void)n;
+    (void)valor;
+    return -1;
+}
+
+int buscar_ocurrencias(int *arr, int n, int valor, int *posiciones) {
+    /* TODO: implementar */
+    (void)arr;
+    (void)n;
+    (void)valor;
+    (void)posiciones;
+    return 0;
+}
