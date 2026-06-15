16ec9e235636ca4a0a129fbc5f608d313ad26b20
Sofi-Castro <castrosalassofia@gmail.com>
2026-06-15 17:57:22 -0600

    Elaboración de bubble sort

--- algoritmos.c
+++ algoritmos.c
@@ -14,7 +14,16 @@ void bubble_sort(int *arr, int n) {
     (void)arr;
     (void)n; 
 
-    for (int i=0; i > n, i)
+    for (int i=0; i < (n-2); i++){
+        for (int j=0; j < (n-i-2); j++){
+            if (*(arr+j) > *(arr+j+1)){
+                int tmp = *(arr+j);
+                *(arr+j) = *(arr+j+1);
+                *(arr+j+1) = tmp;
+            }
+        }
+        
+    }
 
 }
 
